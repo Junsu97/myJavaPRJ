cca2ed620ba826d6063b316e33659d6e94d4731d
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-03-15 09:33:35 +0900

    Add files via upload
    소스수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,23 @@
-public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello Java");
+import java.util.Scanner;
 
-        System.out.println("Edit Java Code!!");
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+
+        String scn = in.nextLine();
+
+        System.out.println(scn);
+
+        int x = in.nextInt();
+
+        System.out.println(x);
+
+
+        in.close();
+        System.out.println("dddddd");
+        System.out.println("ddddddd");
     }
 }
