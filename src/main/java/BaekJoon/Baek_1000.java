4f38a299108f76ec99ef2a4c6d54f3682ef07309
Junsu97 <junsoo1230.gmail.com>
2023-03-15 19:37:56 +0900

    백준

---
+++ src/main/java/BaekJoon/Baek_1000.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+/*
+* 문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A+B를 출력한다.*/
+public class Baek_1000 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+
+        System.out.println(a+b);
+    }
+}
