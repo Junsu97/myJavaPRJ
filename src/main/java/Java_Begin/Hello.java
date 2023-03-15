c6ca3dee724e6d4c68b1aa82cb8e44707ba080f6
Junsu97 <junsoo1230@gmail.com>
2023-03-15 09:45:24 +0900

    Java_Begin

---
+++ src/main/java/Java_Begin/Hello.java
@@ -0,0 +1,27 @@
+package Java_Begin;
+
+import java.util.Scanner;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Java_Begin.Hello World");
+        Scanner in = new Scanner(System.in);
+
+        String scn = in.nextLine();
+
+        System.out.println(scn);
+
+        int x = in.nextInt();
+
+        System.out.println(x);
+
+
+        in.close();
+        System.out.println("dddddd");
+        System.out.println("ddddddd");
+        System.out.println("dddddddddddddddddddd");
+        System.out.println("dddd");
+    }
+}
--- src/main/java/Java_Begin/Hello2.java
+++ src/main/java/Java_Begin/Hello2.java
@@ -1,3 +1,5 @@
+package Java_Begin;
+
 public class Hello2 {
     public static void main(String[] args){
         System.out.println("두번째 생성한 파일 ");
