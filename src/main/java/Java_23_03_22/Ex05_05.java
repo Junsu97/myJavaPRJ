744afeee61babf034cb43487addd5000844b66a6
Junsu97 <junsoo1230@gmail.com>
2023-03-22 11:47:28 +0900

    예제추가

---
+++ src/main/java/Java_23_03_22/Ex05_05.java
@@ -0,0 +1,17 @@
+package Java_23_03_22;
+
+public class Ex05_05 {
+    public static void main(String[] args) {
+        int a = 200;
+
+        if(a < 100){
+            System.out.println("100보다 작군요..");
+            System.out.println("참이면 이 문장도 보이겠죠?");
+        } else {
+            System.out.println("100보다 크군요..");
+            System.out.println("거짓이면 이 문장도 보이겠죠?");
+        }
+
+        System.out.println("프로그램 끝!");
+    }
+}
---
+++ src/main/java/Java_23_03_22/Ex05_06.java
@@ -0,0 +1,20 @@
+package Java_23_03_22;
+
+import java.util.Scanner;
+
+public class Ex05_06 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a;
+
+        System.out.print("정수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if((a%2) == 0){
+            System.out.println("짝수를 입력했군요..");
+        }else{
+            System.out.println("홀수를 입력했군요..");
+        }
+    }
+}
