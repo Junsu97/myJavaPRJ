15bc169ef30b4d5d510b0feccc7101032b73b644
Junsu97 <junsoo1230@gmail.com>
2023-03-29 11:14:19 +0900

    예제추가

---
+++ src/main/java/Java_23_03_29/Ex06_07.java
@@ -0,0 +1,13 @@
+package Java_23_03_29;
+
+public class Ex06_07 {
+    public static void main(String[] args) {
+        int hap;
+        int i;
+
+//        for(i = 1; i <= 10; i++){
+//            hap = hap + i;
+//        }
+//        System.out.printf("1 에서 10까지의 합: %d \n",hap);
+    }
+}
