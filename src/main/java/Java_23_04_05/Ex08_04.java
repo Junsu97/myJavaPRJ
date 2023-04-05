8806ab3430f64acc50b7800b38e9ea4c64fd4bf7
Junsu97 <junsoo1230@gmail.com>
2023-04-05 11:03:49 +0900

    예제추가

---
+++ src/main/java/Java_23_04_05/Ex08_04.java
@@ -0,0 +1,12 @@
+package Java_23_04_05;
+
+public class Ex08_04 {
+    public static void main(String[] args) {
+        int aa[] = {100,200,300,400};
+        int bb[] = new int[] {100,200,300};
+
+        int cc[];
+        cc = new int[] {100,200};
+        int[] dd = new int[1];
+    }
+}
