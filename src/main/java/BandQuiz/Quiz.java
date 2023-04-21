1086273b12aef8d1283b6fe8c3453c50fa5e0f02
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-04-21 23:20:48 +0900

    .

--- src/main/java/BandQuiz/Quiz.java
+++ src/main/java/BandQuiz/Quiz.java
@@ -3,7 +3,7 @@ public class Quiz {
     public static void main(String[] args) {
         for(int i = 2,j =1; i < 10; j++){
             System.out.println(i + "x" + j + "=" + (i*j));
-            if(j == 10 ){
+            if(j == 9 ){
                 i++;
                 j = 0;
             }
