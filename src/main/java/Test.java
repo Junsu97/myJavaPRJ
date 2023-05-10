4b20f188f37499d7781936a720a46f2e9a77e6b1
Junsu97 <junsoo1230@gmail.com>
2023-05-10 09:28:55 +0900

    .

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,19 +1,13 @@
 public class Test {
     public static void main(String[] args){
-        int dan = 2;
-        int gop = 1;
+        String log = "123.123.123.123 - - [12/Apr/2018:17:03:50 +0900] \"GET /api/aaaa HTTP/1.1\" 200 34 1468 \"https://m.naver.com\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216 NAVER(inapp; search; 580; 8.6.3; 7)\" ";
 
-        for(; ; gop++){
-            if(dan < 10){
-                System.out.println(dan + "x" + gop + "=" + dan*gop);
-                if(gop == 9){
-                    dan++;
-                    gop = 0;
-                    System.out.println();
-                }
-            }else{
-                break;
-            }
-        }
+        String[] spritStr = log.split(" ");   // 공백 기준으로 분리
+        String proto = spritStr[5].replaceAll("\"","");
+         /*공백 기준으로 분리하면 "GET 이 출력 되기때문에
+           replaceAll을 통해 " 를 공백으로 치환
+        */
+
+        System.out.println(proto);
     }
 }
